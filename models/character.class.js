961063b89c5114e7be1c09e8931f2d79371d5de4
Michael Fiebelkorn <michael.fiebelkorn@gmail.com>
2024-11-17 12:45:46 +0100

    structure improved

--- models/character.class.js
+++ models/character.class.js
@@ -4,46 +4,18 @@ class Character extends MovableObject {
   //height = 240;
   x = 80;
   y = 140;
-  IMAGES_IDLE = [
-    './img/2_character_pepe/1_idle/idle/I-1.png',
-    './img/2_character_pepe/1_idle/idle/I-2.png',
-    './img/2_character_pepe/1_idle/idle/I-3.png',
-    './img/2_character_pepe/1_idle/idle/I-4.png',
-    './img/2_character_pepe/1_idle/idle/I-5.png',
-    './img/2_character_pepe/1_idle/idle/I-6.png',
-    './img/2_character_pepe/1_idle/idle/I-7.png',
-    './img/2_character_pepe/1_idle/idle/I-8.png',
-    './img/2_character_pepe/1_idle/idle/I-9.png',
-    './img/2_character_pepe/1_idle/idle/I-10.png'
-  ];
-
-  IMAGES_LONG_IDLE = [
-    './img/2_character_pepe/1_idle/long_idle/I-11.png',
-    './img/2_character_pepe/1_idle/long_idle/I-12.png',
-    './img/2_character_pepe/1_idle/long_idle/I-13.png',
-    './img/2_character_pepe/1_idle/long_idle/I-14.png',
-    './img/2_character_pepe/1_idle/long_idle/I-15.png',
-    './img/2_character_pepe/1_idle/long_idle/I-16.png',
-    './img/2_character_pepe/1_idle/long_idle/I-17.png',
-    './img/2_character_pepe/1_idle/long_idle/I-18.png',
-    './img/2_character_pepe/1_idle/long_idle/I-19.png',
-    './img/2_character_pepe/1_idle/long_idle/I-20.png'
-  ];
-
-  IMAGES_WALKING = [
-    './img/2_character_pepe/2_walk/W-21.png',
-    './img/2_character_pepe/2_walk/W-22.png',
-    './img/2_character_pepe/2_walk/W-23.png',
-    './img/2_character_pepe/2_walk/W-24.png',
-    './img/2_character_pepe/2_walk/W-25.png',
-    './img/2_character_pepe/2_walk/W-26.png'
-  ];
+  IMAGES_IDLE = PEPE_IMAGES['IMAGES_IDLE'];
+  IMAGES_LONG_IDLE = PEPE_IMAGES['IMAGES_LONG_IDLE'];
+  IMAGES_WALKING = PEPE_IMAGES['IMAGES_WALKING'];
+  IMAGES_JUMP = PEPE_IMAGES['IMAGES_JUMP'];
+  IMAGES_HURT = PEPE_IMAGES['IMAGES_HURT'];
+  IMAGES_DEAD = PEPE_IMAGES['IMAGES_DEAD'];
+  
   world;    // reference between character and world, declared in method setWorld() in class World, character can now use all variables from world
   speed = 4;
   idleTime = 0;
   timeDiff;
   walking_sound = new Audio('./audio/running.mp3');
-
 
 
   constructor(){
@@ -143,10 +115,7 @@ class Character extends MovableObject {
   playIdleAnimation(arr){
     let intervalIdle =
       setInterval(() => {
-        let i = this.currentImage % arr.length;   // iteriert mit Modulo durch das Array und fängt am Ende wieder bei 0 an
-        let path = arr[i];
-        this.img = this.imageCache[path];
-        this.currentImage++;
+        this.playAnimation(arr);
         if(this.validKeyPressed()){
           clearInterval(intervalIdle);
           this.loadImage('./img/2_character_pepe/2_walk/W-21.png');
